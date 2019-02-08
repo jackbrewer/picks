706fa98759041a580b514f715543a26f9a52053c
Jack Brewer <jack@jackbrewer.co.uk>
2019-02-08 11:36:17 +0000

    feat(storybook): update config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,38 @@
-import { configure } from '@storybook/react'
+import { addDecorator, configure } from '@storybook/react'
+import { withOptions } from '@storybook/addon-options'
+import { withInfo } from '@storybook/addon-info'
 
 import '../src/component/App/App.module.scss'
 
 const req = require.context('../src', true, /stories\.jsx$/)
+
 const loadStories = () => {
   req.keys().forEach(filename => req(filename))
 }
+
+// addon-options
+addDecorator(
+  withOptions({
+    name: 'Backline',
+    url: '/'
+  })
+)
+
+// addon-info
+addDecorator(
+  withInfo({
+    inline: false,
+    styles: {
+      button: {
+        topRight: {
+          borderRadius: '5px 0 0 0',
+          bottom: 0,
+          top: 'auto',
+          zIndex: 99998
+        }
+      }
+    }
+  })
+)
 
 configure(loadStories, module)

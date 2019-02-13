3444cad0dcdca2b01a5498e6439ac54c91f6f4b1
Jack Brewer <jack@jackbrewer.co.uk>
2019-02-13 09:47:24 +0000

    feat(BackgroundImage): adds component

---
+++ src/component/Primitive/BackgroundImage/index.jsx
@@ -0,0 +1,44 @@
+import React, { PureComponent } from 'react'
+import { bool, number, oneOf, string } from 'prop-types'
+import classNames from 'classnames'
+
+import styles from './BackgroundImage.module.scss'
+
+class BackgroundImage extends PureComponent {
+  render() {
+    const { alt, color, fillContainer, position, ratio, size, src } = this.props
+    const formattedRatio = parseFloat((ratio * 100).toFixed(5))
+
+    return (
+      <div
+        className={classNames(
+          styles.BackgroundImage,
+          fillContainer && styles.fillContainer
+        )}
+        style={{
+          backgroundImage: `url(${src})`,
+          ...(color && { backgroundColor: color }),
+          ...(position && { backgroundPosition: position }),
+          ...(ratio && { paddingBottom: `${formattedRatio}%` }),
+          ...(size && { backgroundSize: size })
+        }}
+        {...alt !== '' && {
+          role: 'img',
+          'aria-label': alt
+        }}
+      />
+    )
+  }
+}
+
+BackgroundImage.propTypes = {
+  alt: string.isRequired,
+  color: string,
+  fillContainer: bool,
+  position: string,
+  ratio: number,
+  size: string,
+  src: string.isRequired
+}
+
+export default BackgroundImage

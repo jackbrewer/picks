fc48113a6f3d4141fdc407297e32c238a67cefc7
Jack Brewer <jack@jackbrewer.co.uk>
2019-02-13 18:07:07 +0000

    feat(storybook): misc tweaks

--- .storybook/config.js
+++ .storybook/config.js
@@ -33,11 +33,13 @@ addDecorator(
         }
       },
       infoStory: {
-        border: '1px solid rgb(238, 238, 238)',
-        background: 'rgb(250, 250, 250)',
-        borderRadius: '4px',
-        padding: '20px',
-        margin: '10px 40px'
+        backgroundColor: 'rgb(255, 255, 255)',
+        backgroundImage:
+          'linear-gradient(to bottom, rgb(238, 238, 238) 0px, rgb(238, 238, 238) 1px, rgb(250, 250, 250) 1px, rgb(250, 250, 250) calc(100% - 1px), rgb(238, 238, 238) calc(100% - 1px), rgb(238, 238, 238) 100%)',
+        backgroundPosition: '50% 50%',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'calc(100% - 80px) 100%',
+        padding: '20px 60px'
       },
       infoBody: {
         border: 0,

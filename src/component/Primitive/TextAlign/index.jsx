0877f1b564e91be04cf0024c9a74e2c38717f494
Jack Brewer <jack@jackbrewer.co.uk>
2019-02-13 06:43:40 +0000

    fix(TextAlign): tweak formatting to match other components

--- src/component/Primitive/TextAlign/index.jsx
+++ src/component/Primitive/TextAlign/index.jsx
@@ -5,17 +5,23 @@ import { bool, node } from 'prop-types'
 import styles from './TextAlign.module.scss'
 
 class TextAlign extends PureComponent {
+  getAlignmentType() {
+    const { center, justify, left, right } = this.props
+    if (center) return 'center'
+    if (justify) return 'justify'
+    if (left) return 'left'
+    if (right) return 'right'
+  }
+
   render() {
-    const { children, center, left, right, justify } = this.props
+    const { children } = this.props
+    const alignmentType = this.getAlignmentType()
 
     return (
       <div
         className={classNames(
           styles.TextAlign,
-          center && styles.center,
-          left && styles.left,
-          right && styles.right,
-          justify && styles.justify
+          alignmentType && styles[alignmentType]
         )}
       >
         {children}
@@ -27,9 +33,9 @@ class TextAlign extends PureComponent {
 TextAlign.propTypes = {
   children: node.isRequired,
   center: bool,
+  justify: bool,
   left: bool,
-  right: bool,
-  justify: bool
+  right: bool
 }
 
 export default TextAlign

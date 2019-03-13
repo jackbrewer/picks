2730fa5fc42a3292d2cea7b0ac6ccbbd905a3afa
Jack Brewer <jack@jackbrewer.co.uk>
2019-03-13 10:58:34 +0000

    feat(setup): import base css in custom _app

---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import App from 'next/app'
+
+import '../src/scss/base.scss'
+
+export default App
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import DefaultLayout from '../src/component/Layout/DefaultLayout'
 
-const IndexPage = () => (
-  <DefaultLayout>
-    <h1>Example content</h1>
-  </DefaultLayout>
-)
+const IndexPage = () => <h1>Example content</h1>
 
 export default IndexPage

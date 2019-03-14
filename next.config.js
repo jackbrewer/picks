499fea780e9643891825731c373bfaa9fa429e83
Jack Brewer <jack@jackbrewer.co.uk>
2019-03-14 17:52:18 +0000

    feat(setup): adds webpack-bundle-analyzer scripts

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withPlugins = require('next-compose-plugins')
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
 const withSize = require('next-size')
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
+
 const path = require('path')
 
 const createSvgLoader = require('./config/webpack/svg-loader')
@@ -36,5 +38,22 @@ module.exports = withPlugins([
       }
     }
   ],
-  [withSize]
+  [withSize],
+  [
+    withBundleAnalyzer,
+    {
+      analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: 'static',
+          reportFilename: '../.bundle-analyzer/server.html'
+        },
+        browser: {
+          analyzerMode: 'static',
+          reportFilename: '../.bundle-analyzer/browser.html'
+        }
+      }
+    }
+  ]
 ])

b3b4d097101f07a70f61d9e3ea2af2a372ac68f3
Jack Brewer <jack@jackbrewer.co.uk>
2019-03-26 12:47:08 +0000

    feat(PasswordControl): adds component

---
+++ src/component/Primitive/PasswordControl/PasswordControl.stories.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import PasswordControl from '.'
+
+const stories = storiesOf('Form/PasswordControl', module)
+
+stories.add('Info', () => <PasswordControl name="password" />, {
+  info: {
+    inline: true,
+    text: `
+      Starter component which can be duplicated and used as a starting point
+      for others.
+
+      Use this first story to add documentation and show off the most
+      interesting/useful component iteration. Use subsequent stories to detail
+      all component permutations.
+    `
+  }
+})
+
+stories.add('Password type (default)', () => (
+  <PasswordControl name="password" value="1234" />
+))
+
+stories.add('Text type (forced)', () => (
+  <PasswordControl name="password" value="1234" type="text" />
+))
---
+++ src/component/Primitive/PasswordControl/index.jsx
@@ -0,0 +1,44 @@
+import React, { useRef, useState } from 'react'
+import { oneOf } from 'prop-types'
+
+import DecoratedTextControl from '../DecoratedTextControl'
+import IconButton from '../IconButton'
+
+const PasswordControl = props => {
+  const { type, ...other } = props
+  const [toggleType, setType] = useState(props.type)
+  const controlEl = useRef(null)
+
+  const handleToggleType = () => {
+    setType(toggleType === 'password' ? 'text' : 'password')
+    controlEl && controlEl.current.focus()
+  }
+
+  const isPasswordVisible = toggleType === 'text'
+
+  return (
+    <DecoratedTextControl
+      {...other}
+      type={toggleType}
+      controlRef={controlEl}
+      after={
+        <IconButton
+          icon={isPasswordVisible ? 'visibility' : 'visibility-off'}
+          a11yText={isPasswordVisible ? 'Hide password' : 'Reveal password'}
+          onClick={handleToggleType}
+        />
+      }
+      afterInteractive
+    />
+  )
+}
+
+PasswordControl.defaultProps = {
+  type: 'password'
+}
+
+PasswordControl.propTypes = {
+  type: oneOf(['password', 'text'])
+}
+
+export default PasswordControl

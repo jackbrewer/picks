7e888ef4424c02b0c2dc4de2f9572d746c30974a
Jack Brewer <jack@jackbrewer.co.uk>
2019-04-16 11:27:58 +0100

    fix(Container): tweak stories

--- src/component/Primitive/Container/Container.stories.jsx
+++ src/component/Primitive/Container/Container.stories.jsx
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react'
 
 import Container from '.'
 
-const stories = storiesOf('Unsorted/Container', module)
+const stories = storiesOf('Core/Container', module)
 
 const content =
   'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Maecenas faucibus mollis interdum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.'

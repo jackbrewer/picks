a4d2e96cf499de80ea5f6036a1a14be848da0ca8
Jack Brewer <jack@jackbrewer.co.uk>
2019-04-17 10:41:14 +0100

    feat(date-formatter): adds lib function

---
+++ src/lib/date-formatter.js
@@ -0,0 +1,18 @@
+import { format as formatFn } from 'date-fns'
+
+const formats = {
+  LT: 'p',
+  LTS: 'pp',
+  L: 'd MMM yyyy',
+  LL: 'd MMMM yyyy',
+  LLL: 'd MMMM yyyy p',
+  LLLL: 'iiii d MMMM yyyy p',
+  ISO: `yyyy-MM-dd'T'HH:mm:ssZ`
+}
+
+const dateFormatter = (date, format = 'LLL') => {
+  if (!date) return ''
+  return formatFn(date, `${formats[format]}`)
+}
+
+export default dateFormatter

c441e3dc5a5a390193e7de7e26bf65680fc615d8
Paul Serby <paul.serby@clock.co.uk>
2019-11-13 15:10:52 +0000

    feat(server): Add a simple server example

---
+++ pages/movies.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import fetch from 'isomorphic-unfetch'
+import Container from '@/component/Primitive/Container'
+import List from '@/component/Primitive/List'
+import SpacedList from '@/component/Primitive/SpacedList'
+import Type from '@/component/Primitive/Type'
+import { shape, string } from 'prop-types'
+
+const movieShape = shape({ name: string })
+
+const MoviesPage = ({ movies }) => (
+  <Container size="medium" gutter center>
+    <SpacedList>
+      <Type element="h2" size="display1">
+        Movies Page
+      </Type>
+      <List>
+        {movies.map(movie => (
+          <li key={movie.name}>{movie.name}</li>
+        ))}
+      </List>
+    </SpacedList>
+  </Container>
+)
+
+MoviesPage.propTypes = {
+  movies: movieShape
+}
+
+MoviesPage.getInitialProps = async () => {
+  const response = await fetch('http://localhost:3000/api/movies')
+  if (response.status === 200) {
+    return { movies: await response.json() }
+  }
+  return { movies: [] }
+}
+
+export default MoviesPage

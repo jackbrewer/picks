8eea8e95d2ee9d8fa28ae40ab9fbe48e58ee4c96
Jack Brewer <jack@jackbrewer.co.uk>
2020-02-13 16:36:13 +0000

    Storybook upgrades

---
+++ .storybook/backlineTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create'
+
+// Reference: https://storybook.js.org/docs/configurations/theming/
+
+export default create({
+  base: 'light',
+  brandTitle: 'Backline'
+})

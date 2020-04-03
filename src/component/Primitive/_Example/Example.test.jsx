c884f5674bfd65dbe37be4df54a3d5c031c6bd1e
Jack Brewer <jack@jackbrewer.co.uk>
2020-04-03 12:10:30 +0100

    feat(Example): update default tests

--- src/component/Primitive/_Example/Example.test.jsx
+++ src/component/Primitive/_Example/Example.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import validatePropTypes from 'validate-prop-types'
 import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import Example from '.'
 
 const requiredProps = () => ({ children: 'Default content' })
